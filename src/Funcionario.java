805b2b85b5c841f67a3e846c134971908bf28934
anaakaaro <aanaakaarolina@gmail.com>
2025-05-16 19:51:20 -0300

    adição de getters e setters na classe funcionário

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -12,6 +12,40 @@ public class Funcionario {
         this.cargo = cargo;
     }
 
+    //getters
+    public String getNome(){
+        return nome;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+
+    public String getSenha(){
+        return senha;
+    }
+
+    public String getCargo(){
+        return cargo;
+    }
+
+    //setters
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setLogin(String login){
+        this.login = login;
+    }
+
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+
+    public void setCargo(String cargo){
+        this.cargo = cargo;
+    }
+
     public void exibirFuncionario(){
         System.out.println("Nome: " + nome);
         System.out.println("Login: " + login);
